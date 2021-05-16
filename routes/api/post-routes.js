e01e300889fdb2f9a59cb7e1330cbb88bb7f8051
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-05-16 13:46:34 -0400

    test

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 const { route } = require('./user-routes');
 
 //get all users
@@ -58,6 +58,15 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+//PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => res.json(err));
 });
 router.put('/:id', (req, res) => {
     Post.update(
