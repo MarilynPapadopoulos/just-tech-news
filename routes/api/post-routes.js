39c9cd129b8e262642db0c2778af899d757775df
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-05-18 11:25:32 -0400

    added ability to add comments to posts

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 
 //get all users
 router.get('/', (req, res) => {
-    console.log('=====================');
     Post.findAll({
+        order:[['created_at', 'DESC']],
         attributes: [
             'id', 
             'post_url', 
@@ -14,8 +14,15 @@ router.get('/', (req, res) => {
             'created_at',
                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                } 
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -40,12 +47,20 @@ router.get('/:id', (req, res) => {
             'created_at',
                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
             ],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    } 
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
     })
     .then(dbPostData => {
         if(!dbPostData) {
