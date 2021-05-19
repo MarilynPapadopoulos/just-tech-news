9975f4a9d8682bd898040256ccbe1085f3918576
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-05-19 09:49:16 -0400

    connected homepage

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
+
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));  
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
+const sequelize = require('./config/connection');
+
+const routes = require('./controllers');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));  
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 //turn on routes
 app.use(routes);
+
+
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
